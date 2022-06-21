346b8e25239d97d289c7e6a52d003ea2101b344a
rai10 <saurabhrai265@gmail.com>
2022-06-22 03:32:44 +0530

    slider added and style updated

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -8,10 +8,14 @@ function Header() {
       const current = new Date();
   const date = `${current.getDate()}/${current.getMonth()}/${current.getFullYear()}`;
   return (
-    <div className='flex justify-around items-center w-full p-5 '>
+    <div className='flex justify-around items-center w-full p-2.5'>
         <div className='w-1/2  pl-3'>
 
-            <p className=''><a href="#" className='cursor-pointer'>Meet</a></p>
+            <div className='flex  space-x-1'><img src="https://www.gstatic.com/meet/google_meet_horizontal_wordmark_2020q4_1x_icon_124_40_2373e79660dabbf194273d27aa7ee1f5.png" alt="logo" /><a href="#" className='cursor-pointer top-2'
+            style={{
+                fontSize:'22px'
+            }}
+            >Meet</a></div>
         </div>
         <div className='flex items-center w-1/2 justify-end pl-3'>
             <div className='w-auto'><p>{date}</p></div>
--- src/components/slider.tsx
+++ src/components/slider.tsx
@@ -3,27 +3,47 @@ import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
 import "./styles.css";
-import { Navigation, Pagination, Mousewheel, Keyboard } from "swiper";
+import { Navigation, Pagination, Keyboard } from "swiper";
 
 function Slider() {
    
     
   return (
-    <div className=' w-1/2 h-[88.6vh]'>
+    <div className=' w-1/2 h-[88.6vh] flex '>
     <>
       <Swiper
         cssMode={true}
         navigation={true}
         pagination={true}
-        mousewheel={true}
         keyboard={true}
-        modules={[Navigation, Pagination, Mousewheel, Keyboard]}
-        className="mySwiper bg-red-500"
+        modules={[Navigation, Pagination, Keyboard]}
+        className="mySwiper"
       >
-        <SwiperSlide>Slide 1</SwiperSlide>
-        <SwiperSlide>Slide 2</SwiperSlide>
-        <SwiperSlide>Slide 3</SwiperSlide>
-        <SwiperSlide>Slide 4</SwiperSlide>
+        <div>
+          <SwiperSlide>
+            <img src="https://www.gstatic.com/meet/user_edu_get_a_link_light_90698cd7b4ca04d3005c962a3756c42d.svg" alt="pic01"  />
+            <p>Get alink that you can share</p>
+            <p>Click New meeting to get a link that you can send</p>
+            <p>to people that you want to meet with</p>
+        </SwiperSlide>
+        </div>
+        
+        <SwiperSlide>
+          <img src="https://www.gstatic.com/meet/user_edu_brady_bunch_light_81fa864771e5c1dd6c75abe020c61345.svg" alt="pic02" />
+          <p>See everyone together</p>
+          <p>To see more people at the same time, go to Change</p>
+          <p>layout in the More options menu</p>
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://www.gstatic.com/meet/user_edu_scheduling_light_b352efa017e4f8f1ffda43e847820322.svg" alt="pic04" />
+          <p>Plan ahead</p>
+          <p>Click New meeting to schedule meetings in Google</p>
+          <p>Calender and send invitations to participants</p>
+        </SwiperSlide>
+        <SwiperSlide className="flex flex-rows">
+            <img src="https://www.gstatic.com/meet/user_edu_safety_light_e04a2bbb449524ef7e49ea36d5f25b65.svg" alt="pic04" />
+            <p>Your meeting is safe</p>
+        </SwiperSlide>
       </Swiper>
     </>
     </div>
