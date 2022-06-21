47a99c606fd944c31f418bfa21b87d9f07446672
rai10 <saurabhrai265@gmail.com>
2022-06-22 01:58:39 +0530

    slider js added

--- src/components/slider.tsx
+++ src/components/slider.tsx
@@ -1,7 +1,33 @@
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "./styles.css";
+import { Navigation, Pagination, Mousewheel, Keyboard } from "swiper";
 
 function Slider() {
+   
+    
   return (
-    <div className="bg-green-500 w-1/2 h-[88.6vh]">slider</div>
+    <div className=' w-1/2 h-[88.6vh]'>
+    <>
+      <Swiper
+        cssMode={true}
+        navigation={true}
+        pagination={true}
+        mousewheel={true}
+        keyboard={true}
+        modules={[Navigation, Pagination, Mousewheel, Keyboard]}
+        className="mySwiper bg-red-500"
+      >
+        <SwiperSlide>Slide 1</SwiperSlide>
+        <SwiperSlide>Slide 2</SwiperSlide>
+        <SwiperSlide>Slide 3</SwiperSlide>
+        <SwiperSlide>Slide 4</SwiperSlide>
+      </Swiper>
+    </>
+    </div>
+   
   )
 }
 
